d09146689cf8935712a89af10826bcac179464ef
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-09-17 21:47:25 +0530

    initial commit

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,103 @@
+import { useState } from 'react';
+import { useAuth } from '../utils/hooks/authHook';
+import { Link } from 'react-router-dom';
+
+const RegisterPage = () => {
+  const [credentials, setCredentials] = useState({
+    name: '',
+    email: '',
+    password1: '',
+    password2: '',
+  });
+
+  const { handleRegister } = useAuth();
+
+  const handleInputChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setCredentials({ ...credentials, [name]: value });
+  };
+
+  return (
+    <div className="auth--container">
+      <div className="form--wrapper">
+        <form
+          onSubmit={(e) => {
+            handleRegister(e, credentials);
+          }}
+        >
+          <div className="field--wrapper">
+            <label>Name</label>
+            <input
+              required
+              type="text"
+              name="name"
+              value={credentials.name}
+              placeholder="John Doe"
+              onChange={(e) => {
+                handleInputChange(e);
+              }}
+            />
+          </div>
+
+          <div className="field--wrapper">
+            <label>Email</label>
+            <input
+              required
+              type="email"
+              name="email"
+              placeholder="john@example.com"
+              value={credentials.email}
+              onChange={(e) => {
+                handleInputChange(e);
+              }}
+            />
+          </div>
+
+          <div className="field--wrapper">
+            <label>Password</label>
+            <input
+              required
+              type="password"
+              name="password1"
+              placeholder="password123"
+              value={credentials.password1}
+              onChange={(e) => {
+                handleInputChange(e);
+              }}
+            />
+          </div>
+
+          <div className="field--wrapper">
+            <label>Confirm password</label>
+            <input
+              required
+              type="password"
+              name="password2"
+              placeholder="password123"
+              value={credentials.password2}
+              onChange={(e) => {
+                handleInputChange(e);
+              }}
+            />
+          </div>
+
+          <div className="field--wrapper">
+            <input
+              className="btn btn--lg btn--main"
+              type="submit"
+              value="Register"
+            />
+          </div>
+        </form>
+
+        <p>
+          Already have an account? <Link to="/login">Login</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterPage;
