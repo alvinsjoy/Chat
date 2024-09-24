d38e5b7e2a3c120d313f7f59b9927a622491486b
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-09-24 18:59:02 +0530

    Refactor PrivateRoutes to allow access to the /verify route

--- src/utils/PrivateRoutes.jsx
+++ src/utils/PrivateRoutes.jsx
@@ -1,9 +1,13 @@
-import { Outlet, Navigate } from 'react-router-dom';
+import { Outlet, Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from './hooks/authHook';
 
 const PrivateRoutes = () => {
   const { user } = useAuth();
+  const location = useLocation();
 
+  if (location.pathname === '/verify') {
+    return <Outlet />;
+  }
   if (!user) {
     return <Navigate to="/login" />;
   }
